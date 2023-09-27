bf7e9d668fd9acf534f7d1085cead441ec306726
m <bugmuha@gmail.com>
2023-09-27 17:04:28 +0300

    Automated the unit title.

--- grade9/views.py
+++ grade9/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 from grade9.models import *
 
 
-# Create your views here.
-def get_chapter(text):
-    chapter = text.split(" ")[1]
-    return int(chapter)
-
-
 def main(request):
-    chapters = ['1s', '2s', '1a', '2a', '3s', '3a']
-    # chapters = ['1s', '1a']
+    chapters = ['1s', '2s', '1a', '2a', '3s', '3a','4a','4s','5s']
+    # chapters = ['3s', '3a', '2a', '2s']
+    chapters = ['1s', '1a']
+    grade = 7
+    grades = ['صفر', 'صفر', 'صفر', 'صفر', 'صفر', 'صفر', 'صفر', 'السابع', 'الثامن', 'التاسع', 'العاشر', 'الحادي عشر',
+              'الثاني عشر', ]
     s = set()
     for ch in chapters:
         s.add(int((ch[0])))
@@ -37,23 +35,40 @@ def main(request):
     if chapter_title[-2] == "و":
         chapter_title = chapter_title[:-3]
 
-    passage_with_choices = Text.objects.filter(type='choices', chapter__number__in=chapters).order_by('?').first()
+    passage_with_choices = Text.objects.filter(type='choices', chapter__number__in=chapters,
+                                               chapter__grade__grade=grade).order_by(
+        '?').first()
     questions_for_the_first_passage = TextQuestion.objects.filter(text_id=passage_with_choices.id)[:5]
-    tf_passage = Text.objects.filter(type="tof", chapter__number__in=chapters).order_by('?').first()
-    questions_for_the_second_passage = TFQuestions.objects.filter(text_id=tf_passage.id).order_by('?')[:5]
-    c = CTMistake.objects.filter(chapter__number__in=chapters).order_by('?')[:4]
-    vocabularies = Multiples.objects.filter(type='vocabulary', chapter__number__in=chapters).order_by('?')[:4]
-    vocabularies_pks = vocabularies.values_list('pk', flat=True)
-    grammars = Multiples.objects.filter(chapter__number__in=chapters).exclude(pk__in=vocabularies_pks).order_by('?')[
-               :16]
+    tf_passage = Text.objects.filter(type="tof", chapter__number__in=chapters, chapter__grade__grade=grade).order_by(
+        '?').first()
+    questions_for_the_second_passage = TFQuestions.objects.filter(text_id=tf_passage.id,
+                                                                  ).order_by('?')[:5]
+    c = CTMistake.objects.filter(chapter__number__in=chapters, chapter__grade__grade=grade).order_by('?')[:4]
+    vocabularies = Multiples.objects.filter(type='vocabulary', chapter__number__in=chapters,
+                                            chapter__grade__grade=grade).order_by('?')[:4]
 
-    questions = MakeQuestions.objects.filter(chapter__number__in=chapters).order_by('?')[:4]
-    correct = CorrectTheMistake.objects.filter(chapter__number__in=chapters).order_by('?').first()
-    paragraph = Paragraphs.objects.filter(chapter__number__in=chapters).order_by('?').first()
+    x_ids = list(vocabularies.values_list('pk', flat=True))
+    print(list(x_ids))
+    grammars = Multiples.objects.filter(chapter__number__in=chapters, chapter__grade__grade=grade).exclude(
+        type='vocabulary').order_by('?')[:16]
+    # grammars = y.exclude(id__in=x_ids)
+    # grammars = grammars[:16]
+    # grammar__ids = grammars.values_list('id',flat=True)
+    # print(grammar__ids)
+    # result = list(set(x_ids).intersection(grammar__ids))
+    # print(result)
+
+    questions = MakeQuestions.objects.filter(chapter__number__in=chapters, chapter__grade__grade=grade).order_by('?')[
+                :4]
+    correct = CorrectTheMistake.objects.filter(chapter__number__in=chapters, chapter__grade__grade=grade).order_by(
+        '?').first()
+    paragraph = Paragraphs.objects.filter(chapter__number__in=chapters, chapter__grade__grade=grade).order_by(
+        '?').first()
 
     context = {'passage_with_choices': passage_with_choices,
                'chapters': chapter_title,
                'c': c,
+               'grade': grades[grade],
                'questions_for_the_first_passage': questions_for_the_first_passage,
                'tf_passage': tf_passage,
                'questions_for_the_second_passage': questions_for_the_second_passage,
@@ -61,6 +76,6 @@ def main(request):
                'grammars': grammars,
                'questions': questions,
                'correct': correct,
-               'para': paragraph
+               'para': paragraph,
                }
     return render(request, "9thGrade/index2.html", context=context)
